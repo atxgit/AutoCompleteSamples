6ede49b9737579d68f9bfc4289b038bc4447983a
John D Hill <john_d_hill@dell.com>
2014-01-23 09:52:37 -0600

    s

---
+++ AutoCompleteSamples/Controllers/ValuesAPIController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AutoCompleteSamples.Controllers
+{
+    public class ValuesAPIController : ApiController
+    {
+        // GET api/valuesapi
+        public IEnumerable<string> Get([FromUri]string term)
+        {
+            var ret = new string[] {};
+            //System.Threading.Thread.Sleep(2000);
+            if (!string.IsNullOrEmpty(term))
+            {
+                ret = new string[] { term + " Smith", term + " Jones", term + " Baker" };
+            }
+
+            return ret;
+        }
+
+        // GET api/valuesapi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/valuesapi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/valuesapi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/valuesapi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AutoCompleteSamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoCompleteSamples.MvcApplication" Language="C#" %>
