be717a5365c68057835b393f8a81db1dad3d7d76
atxgit <atxgit@hillbox.com>
2014-01-24 15:18:42 -0600

    mobile changes

---
+++ AutoCompleteSamples/TestSlidingMenujqueriUI_deploy.bat
@@ -0,0 +1,3 @@
+copy "C:\Users\John.HILLBOX\Documents\GitHub\AutoCompleteSamples\AutoCompleteSamples\TestSlidingMenuJqueryUI.html" "\\192.168.1.82\d$\WWW\Hillbox\websiteHB\testmenu.html" /Y
+copy "C:\Users\John.HILLBOX\Documents\GitHub\AutoCompleteSamples\AutoCompleteSamples\jquery.ui.touch-punch.js" "\\192.168.1.82\d$\WWW\Hillbox\websiteHB" /Y
+copy "C:\Users\John.HILLBOX\Documents\GitHub\AutoCompleteSamples\AutoCompleteSamples\tick.png" "\\192.168.1.82\d$\WWW\Hillbox\websiteHB" /Y
